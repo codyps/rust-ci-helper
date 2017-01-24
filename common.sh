c4f694811e105b971bbaeaba88ca22b10b016387
Cody P Schafer <dev@codyps.com>
2017-01-23 22:50:18 -0500

    default to empty features

--- common.sh
+++ common.sh
@@ -15,7 +15,7 @@ host=${host_arch}${host_vendor:-}-${host_os}
 PROJECT_NAME="$PROJECT_BASE/${TARGET}/${TRAVIS_RUST_VERSION}"
 
 run_cargo() {
-  if [ -n "$FEATURES" ]; then
+  if [ -n "${FEATURES:-}" ]; then
     cargo "$@" --verbose --target="$TARGET" --features="$FEATURES"
   else
     cargo "$@" --verbose --target="$TARGET"
