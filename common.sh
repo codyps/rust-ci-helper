a3fa617cb55074435a721aac2ce2be1b2f694e17
Cody P Schafer <dev@codyps.com>
2017-01-23 22:54:27 -0500

    rework target cc handling

--- common.sh
+++ common.sh
@@ -22,3 +22,35 @@ run_cargo() {
   fi
 }
 
+export TARGET_CC=cc
+RUN_COMPAT=true
+if [ "$host" != "$TARGET" ]; then
+  CROSS=true
+  RUN_COMPAT=false
+  # if the arch is the same, attempt to use the host compiler.
+  # FIXME: not always correct to do so
+  # Also try to use the host compiler if the arch has a 32vs64 bit differenct
+  # FIXME: might also need to check that OS has a reasonable match
+  if [ "$TARGET_ARCH" = arm ]; then
+    export TARGET_CC="${TARGET_ARCH}-${TARGET_OS}-gcc"
+  elif [ "$host_arch" != "$TARGET_ARCH" ] && \
+    ! ( [ "$host_arch" == x86_64 ] && [ "$TARGET_ARCH" == i686 ] ); then
+    export TARGET_CC=$TARGET-gcc
+  fi
+
+  if [ "$host_arch" == x86_64 ] && [ "$TARGET_ARCH" == i686 ]; then
+    RUN_COMPAT=true
+  fi
+
+  if [ "$TARGET_OS" == linux-musl ]; then
+    RUN_COMPAT=true
+    export TARGET_CC=musl-gcc
+  fi
+
+  if [ "$host_os" = "osx" ]; then
+    brew install gnu-sed --default-names
+  fi
+
+  # NOTE Workaround for rust-lang/rust#31907 - disable doc tests when cross compiling
+  find src -name '*.rs' -type f -exec sed -i -e 's:\(//.\s*```\):\1 ignore,:g' \{\} \;
+fi
--- install.sh
+++ install.sh
@@ -9,17 +9,9 @@ set -ex
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain "$TRAVIS_RUST_VERSION"
 rustup target add "$TARGET"
 
-# Install standard libraries needed for cross compilation
-if [ "$host" != "$TARGET" ]; then
-  if [ "$TARGET" = "arm-unknown-linux-gnueabihf" ]; then
-    # information about the cross compiler
-    arm-linux-gnueabihf-gcc -v
-
-    # tell cargo which linker to use for cross compilation
-    mkdir -p .cargo
-    cat >.cargo/config <<EOF
+# tell cargo which linker to use for cross compilation
+mkdir -p .cargo
+cat >.cargo/config <<EOF
 [target.$TARGET]
-linker = "arm-linux-gnueabihf-gcc"
+linker = "$TARGET_CC"
 EOF
-  fi
-fi
